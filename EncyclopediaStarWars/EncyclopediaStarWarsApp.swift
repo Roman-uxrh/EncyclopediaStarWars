1e77ccf83b8bddfd74fadd4ce9786132f862784b
RRDubina <dubinaroman.dubina@gmail.com>
2023-07-19 11:21:02 +0500

    Initial Commit

---
+++ EncyclopediaStarWars/EncyclopediaStarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncyclopediaStarWarsApp.swift
+//  EncyclopediaStarWars
+//
+//  Created by admin on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct EncyclopediaStarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
